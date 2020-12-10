f6dbc90f8dd53f7e91de48009f83f3101ba7c978
supercrafter333 <justcoolstuff99@gmail.com>
2020-12-10 15:53:11 +0100

    some improvements and bug fixes

--- src/supercrafter333/AllClaimPlotCommand/AllClaimPlotCommand.php
+++ src/supercrafter333/AllClaimPlotCommand/AllClaimPlotCommand.php
@@ -39,7 +39,7 @@ class AllClaimPlotCommand extends PluginBase implements Listener
                 if ($s->hasPermission("allclaimplotcommand.allclaimplot.cmd")) {
                     foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
                         $plotlevels = $myplot->getPlotLevels();
-                        if ($s->getLevel()->getName() == $plotlevel)
+                        if ($s->getLevel()->getName() == $plotlevels)
                     $freePlot = $myplot->getNextFreePlot($s->getLevel()->getName());
                         $myplot->teleportPlayerToPlot($onlinePlayer, $freePlot);
                         $myplot->claimPlot($freePlot, $onlinePlayer->getName());
